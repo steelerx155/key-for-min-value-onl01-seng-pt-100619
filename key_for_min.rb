c91c175a811a6c095609497b774430d0ff0a4a47
Randy Steele <steelerx155@gmail.com>
2019-10-21 17:46:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name_hash = { 
+name_hash = { :blake => 500, :ashley => 2, :adam => 1}
 end
