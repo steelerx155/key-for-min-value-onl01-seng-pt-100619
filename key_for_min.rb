c84c9ac3119ca222d7e5a6b53644f698af6be3c4
Randy Steele <steelerx155@gmail.com>
2019-10-21 18:10:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-name_hash 
 name_hash.each do |name, number|
   binding.pry
   name
