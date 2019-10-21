6b911bec2de0476b446b4c85b44e5fee464d998d
Randy Steele <steelerx155@gmail.com>
2019-10-21 18:16:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,8 @@ def key_for_min_value(name_hash)
   lowest_value = nil
 name_hash.each do |name, number|
   if lowest_value = nil || number < lowest_value
-    lowest_value
+    lowest_value = number
+    
    
   name < size
 end
