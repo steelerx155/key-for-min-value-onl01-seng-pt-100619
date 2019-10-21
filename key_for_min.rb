17c481612c0294bf93c4ae8730c3f9f32cbbe07e
Randy Steele <steelerx155@gmail.com>
2019-10-21 17:50:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
 name_hash = { :blake => 500, :ashley => 2, :adam => 1}
-name_hash.min.collect do |name, number|
+name_hash.collect do |name, number|
   name
 end
 end
